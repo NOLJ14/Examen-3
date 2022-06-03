662d9a9ee3b316bb53fe29b11a4c2cc7908f3340
Fesur <91032302+Fesur@users.noreply.github.com>
2022-06-02 23:06:35 -0400

    Contactos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactosComponent } from './components/contactos/contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'contactos',
+    component:ContactosComponent
 
+  }
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactosComponent } from './components/contactos/contactos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultaComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
